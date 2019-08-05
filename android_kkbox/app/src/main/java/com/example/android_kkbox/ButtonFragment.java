d65a7ba3903119dd8a509ce5cb78e48d52349321
tsung <tsung@localhost.localdomain>
2019-08-05 13:12:30 +0800

    andoird hw, listview in fragment

---
+++ android_kkbox/app/src/main/java/com/example/android_kkbox/ButtonFragment.java
@@ -0,0 +1,68 @@
+package com.example.android_kkbox;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class ButtonFragment extends Fragment {
+    // UI components
+    private Button like_btn, unlike_btn;
+    RetrofitRequest req;
+
+    public ButtonFragment() {
+        // Required empty public constructor
+    }
+
+    public static ButtonFragment newInstance() {
+        ButtonFragment fragment = new ButtonFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.button_fragment, container, false);
+
+        // Initialize UI button
+        like_btn = view.findViewById(R.id.like_btn);
+        unlike_btn = view.findViewById(R.id.unlike_btn);
+
+        like_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+//                req = RetrofitRequest.getInstance();
+//                req.Request_LikeInfo("like");
+                Log.d("click like button", "Click like button");
+                Bundle bundle = new Bundle();
+                bundle.putString("preference", "like");
+
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.main_container, ContentFragment.newInstance(bundle)).commit();
+            }
+        });
+
+        unlike_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                req = RetrofitRequest.getInstance();
+                req.Request_LikeInfo("unlike");
+            }
+        });
+
+        return view;
+    }
+
+}
